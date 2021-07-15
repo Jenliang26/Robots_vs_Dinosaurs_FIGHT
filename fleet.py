6a2421c2095fa4893f5a5c1a729edf0e3d15721b
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-15 17:56:58 -0500

    added self.create_fleet so that the fleet can be called

--- fleet.py
+++ fleet.py
@@ -1,15 +1,16 @@
-from fleet import Robot
+from robot import Robot
 
 class Fleet:
     def __init__(self):
-        self.robot_fleet = []
+        self.robots = []
+        self.create_fleet()
         
     def create_fleet(self):
         robot_one = Robot("Hank")
-        self.robot_fleet.append(robot_one)
+        self.robots.append(robot_one)
         robot_two = Robot("Frank")
-        self.robot_fleet.append(robot_two)
+        self.robots.append(robot_two)
         robot_three = Robot("Mark")
-        self.robot_fleet.append(robot_three)
+        self.robots.append(robot_three)
 
 
